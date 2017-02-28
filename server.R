6435932bcdc6a4963407a55c72fe5a8a65dcd7a1
Matt <brysonm18@mail.wlu.edu>
2017-02-28 12:59:44 -0500

    added graph

--- server.R
+++ server.R
@@ -19,6 +19,9 @@ shinyServer(function(input, output) {
     # You can access the value of the widget with input$text, e.g.
     output$value <- renderPrint({ input$text })
   }
-  p<-
   
+  p<-ggplot(Data, aes(Season, PlusMinusTotal)) + geom_point()
+    print(p)
+    
 })
+  
