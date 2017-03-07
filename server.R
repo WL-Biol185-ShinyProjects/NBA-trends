08419758499708b38fc702ae6dbbd0a7199761ed
Matt <brysonm18@mail.wlu.edu>
2017-03-07 12:49:23 -0500

    continuing work on inputs

--- server.R
+++ server.R
@@ -6,6 +6,11 @@ shinyServer(function(input, output) {
   output$Player<- renderPrint({input$Player})
   output$Season <- renderPrint({ input$Season})
   output$Team <- renderPrint({ input$checkGroup })
-  
+  output$value <- renderPrint({ input$PPG })
+  output$value <- renderPrint({ input$PlusMinus })
+  output$value <- renderPrint({ input$FGPer })
+  output$value <- renderPrint({ input$ThreePrPer })
+  output$value <- renderPrint({ input$PPG })
+  output$value <- renderPrint({ input$All })
 })
   
--- ui.R
+++ ui.R
@@ -4,29 +4,36 @@ library(shiny)
 fluidPage(
   
   # Application title
-  titlePanel("NBA Data"),
+  titlePanel("Filter by"),
   
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
+        actionButton("All", label = "All"),
 
-        textInput("Player", label = h3("Input Player Name"), value = "Enter player name here"),
+        textInput("Player", label = h3("Filter by player"), value = "Enter player name here"),
        
 
-        sliderInput("Season", label = h3("Season Selection"), min = 1996, 
+        sliderInput("Season", label = h3("Filter by season"), sep = "", min = 1996, 
                          max = 2016, value = c(2010,2016)),
 
       
-        checkboxGroupInput("Team", label = h3("Select Team"), 
-                         choices = (Data$Team),
-                         selected = "GS")
+        checkboxGroupInput("Team", label = h3("Filter by team"), 
+                         choices = list("Choice 1"=1),
+                         selected = 1)
 
     ),
  
     
     # Show a plot of the generated distribution
+  
     mainPanel(
-      plotOutput("distPlot")
+ 
+      actionButton("PPG", label = "Points Per Game"),
+      actionButton("PlusMinus", label = "Plus/Minus Score"),
+      actionButton("FGPer", label = "Field Goal Percentage"),
+      actionButton("ThreePtPer", label = "Three Point Percentage"),
+      actionButton("Salary", label = "Salary")
       
     )
   )
