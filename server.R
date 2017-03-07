b2cc87ecf80ad8d8c6a5f655027b1e9cbc0b7ec7
Matt <brysonm18@mail.wlu.edu>
2017-03-07 12:15:03 -0500

    Adding filter inputs

--- server.R
+++ server.R
@@ -3,7 +3,9 @@ library(shiny)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
   
-  output$distPlot <- renderPlot({
+  output$Player<- renderPrint({input$Player})
+  output$Season <- renderPrint({ input$Season})
+  output$Team <- renderPrint({ input$checkGroup })
 
   
   })
--- ui.R
+++ ui.R
@@ -4,20 +4,29 @@ library(shiny)
 fluidPage(
   
   # Application title
-  titlePanel("NBA Plus/Minus Data"),
+  titlePanel("NBA Data"),
   
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-      textInput("text", label = h3("Input Player Name"), value = "Enter player name here"),
-      
-      hr(),
-      fluidRow(column(3, verbatimTextOutput("value")))
+      textInput("Player", label = h3("Input Player Name"), value = "Enter player name here"),
+         # hr(),
+         #fluidRow(column(3, verbatimTextOutput("value")))
+
+       sliderInput("Season", label = h3("Season Selection"), min = 1996, 
+                         max = 2016, value = c(2010,2016))
+        
+        checkboxGroupInput("checkGroup", label = h3("Checkbox group"), 
+                         choices = list("Choice 1" = 1, "Choice 2" = 2, "Choice 3" = 3),
+                         selected = 1),
+
     ),
+ 
     
     # Show a plot of the generated distribution
     mainPanel(
       plotOutput("distPlot")
+      
     )
   )
   
