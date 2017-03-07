db8a7bf483e9ef38d6e3b039e5ae33a0aea4dc1b
jkwilliamson95 <jkwilliamson95@gmail.com>
2017-03-07 12:58:23 -0500

    adding checkbox

--- ui.R
+++ ui.R
@@ -18,9 +18,12 @@ fluidPage(
                          max = 2016, value = c(2010,2016)),
 
       
-        checkboxGroupInput("Team", label = h3("Filter by team"), 
-                         choices = list("Choice 1"=1),
-                         selected = 1)
+        checkboxGroupInput("Team", label = h3("Select Team"), 
+                           choices = ("ATL" = 1, "BKN" = 2, "BOS" = 3, "CHA" = 4, "CHI" = 5, "CLE" = 6, "DAL" = 7, "DEN" = 8, "DET" = 9, "GSW" = 10, "HOU" = 11, "IND" = 12, "LAC" = 13, "LAL" = 14, "MEM" = 15, "MIA" = 16, "MIL" = 17, "MIN" = 18, "NOP" = 19, "NYK"= 20, "OKC"= 21, "ORL" = 22, "PHI" = 23, "PHX" = 24, "POR" = 25, "SAC" = 26, "SAS" = 27, "TOR" = 28, "UTA" = 29, "WAS" = 30 ),
+                           selected = "GSW")
+        
+        hr()
+        fluidRow(column(30, verbatimTextOutput("value")))
 
     ),
  
