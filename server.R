d490ec8c6a9fa45ad52a28ea83aa4a36104c30cd
Matt <brysonm18@mail.wlu.edu>
2017-03-14 13:08:00 -0400

    changed data

--- server.R
+++ server.R
@@ -3,8 +3,7 @@ library(shiny)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) 
   
-  fullStats %>%
-    
+ 
   output$NBAplot <- renderPlot({ggplot(data=fullStats, aes_string(input$XInput, input$YInput, colour=input$ColorBy))+geom_point()})
 
 ) 
