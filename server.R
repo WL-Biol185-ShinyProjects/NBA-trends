d0ca5a6f354495216c8237e2098e25ab70aa898d
Matt <brysonm18@mail.wlu.edu>
2017-03-16 13:10:12 -0400

    things still not working

--- server.R
+++ server.R
@@ -8,21 +8,21 @@ function(input, output){
      renderPlot({
        
        if(input$Player == ""){
-         player <- unique(data.frame(fullStats$PLAYER))
+         player <- fullStats$PLAYER
        } else {
          player <- input$Player
        }
        
        if(input$Team == ""){
-         team <- unique(data.frame(fullStats$TEAM))
+         team <- fullStats$TEAM
        } else {
          team <- input$Team
        }
        
        fullStats %>%
-         filter(player %in% fullStats$PLAYER)
-         filter(Season=input$Season)
-         filter(team %in% fullStats$TEAM)
+         filter(PLAYER %in% player)
+         filter(SEASON %in% input$Season)
+         filter(TEAM %in% team)
 
          ggplot(aes_string(input$XInput, input$YInput, colour=input$ColorBy))+geom_point()
 
