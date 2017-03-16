73a5bd216c76af569bd046cd58b8369ac86c5b81
Matt <brysonm18@mail.wlu.edu>
2017-03-16 12:17:52 -0400

    added if statement and selectize input

--- server.R
+++ server.R
@@ -3,8 +3,17 @@ library(shiny)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) 
   
- 
-  output$NBAplot <- renderPlot({ggplot(data=fullStats, aes_string(input$XInput, input$YInput, colour=input$ColorBy))+geom_point()+geom_smooth()})
+  
+  if(input$Player==""){
+    player<-fullStats$PLAYER
+  } else{
+    player<-players<-input$Player
+  }
+  
+  fullStats %>%
+    filter(player %in% fullStats$PLAYER),
+
+   output$NBAplot <- renderPlot({ggplot(data=fullStats, aes_string(input$XInput, input$YInput, colour=input$ColorBy))+geom_point()+geom_smooth()})
 
 ) 
   
