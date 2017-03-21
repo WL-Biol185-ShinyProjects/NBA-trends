62c00a92c43ec84ffe3db9c50f0c6af3f55725df
Kyle Singerman <kylesingerman@gmail.com>
2017-03-21 12:54:24 -0400

    made mine match matt's

--- server.R
+++ server.R
@@ -18,15 +18,7 @@ function(input, output){
        } else {
          team <- input$Team
        }
-<<<<<<< HEAD
-       
-       fullStats %>%
-         filter(fullStats$PLAYER %in% player) %>%
-         filter(fullStats$SEASON %in% input$Season) %>%
-         filter(fullStats$TEAM %in% team) %>%
-=======
->>>>>>> 839c4641ceef310751331e1b333c28fa9f7ee109
-
+           
        fullStats %>%
          filter(fullStats$PLAYER %in% player, fullStats$SEASON >= input$Season[1], fullStats$SEASON <= input$Season[2], fullStats$TEAM %in% team) %>%
          ggplot(aes_string(input$XInput, input$YInput, colour=input$ColorBy))+geom_point()
