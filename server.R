00770f2180676d9748ee90be3b60a5f0c0d9aaa9
Matt <brysonm18@mail.wlu.edu>
2017-03-21 12:06:51 -0400

    added pipe, changed filter functions

--- server.R
+++ server.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(ggplot2)
 
 
 function(input, output){
@@ -6,23 +7,23 @@ function(input, output){
 
   output$NBAplot <-
      renderPlot({
-       
+
        if(input$Player == ""){
          player <- unique(data.frame(fullStats$PLAYER))
        } else {
          player <- input$Player
        }
-       
+
        if(input$Team == ""){
-         team <- unqique(data.frame(fullStats$TEAM))
+         team <- unique(data.frame(fullStats$TEAM))
        } else {
          team <- input$Team
        }
-       
+
        fullStats %>%
-         filter(fullStats$PLAYER %in% player)
-         filter(fullStats$SEASON %in% input$Season)
-         filter(fullStats$TEAM %in% team)
+         filter(fullStats$PLAYER %in% player) %>%
+         filter(fullStats$SEASON %in% input$Season) %>%
+         filter(fullStats$TEAM %in% team) %>%
 
          ggplot(aes_string(input$XInput, input$YInput, colour=input$ColorBy))+geom_point()
 
