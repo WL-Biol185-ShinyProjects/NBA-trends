e4008241af7a0ddbb51c6b00ff080e5fa50bfafc
Kyle Singerman <kylesingerman@gmail.com>
2017-03-23 12:23:36 -0400

    fixed bracket error

--- server.R
+++ server.R
@@ -6,7 +6,7 @@ function(input, output){
   
   
   output$NBAplot <-
-<<<<<<< HEAD
+
      renderPlot({
 
        if(is.null(input$Player)){
@@ -20,11 +20,7 @@ function(input, output){
        } else {
          team <- input$Team
        }
-<<<<<<< HEAD
-           
-=======
 
->>>>>>> 81068a9a4c2d5db2514ae8f7ed09eaa785729a2e
        fullStats %>%
          filter(
                   fullStats$PLAYER %in% player, 
@@ -32,7 +28,7 @@ function(input, output){
                   fullStats$TEAM %in% team 
                 )%>%
          ggplot(aes_string(input$XInput, input$YInput, colour=input$ColorBy))+geom_point()
-=======
+
     renderPlot({
       
       if(is.null(input$Player)){
@@ -56,9 +52,8 @@ function(input, output){
         ggplot(aes_string(input$XInput, input$YInput, colour=input$ColorBy))+geom_point()
       
     })
+     }
 }
-
->>>>>>> 32db839405660d51246b6a79939cc18f7332df7a
 
 
   
