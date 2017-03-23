7f1b9fa96199cddbf2d12ece47708a0d726d5b15
Matt <brysonm18@mail.wlu.edu>
2017-03-23 13:16:59 -0400

    got 2 of 3 filters working

--- server.R
+++ server.R
@@ -12,25 +12,26 @@ function(input, output){
     renderPlot({
       
       if(is.null(input$Player)){
-        player <- unique(data.frame(fullStats$PLAYER))
+        player <- fullStats$PLAYER
       } else {
         player <- input$Player
       }
       
       if(is.null(input$Team)){
-        team <- unique(data.frame(fullStats$TEAM))
+        team <- fullStats$TEAM
       } else {
         team <- input$Team
       }
       
       fullStats %>%
         filter( 
-        PLAYER %in% player
+        PLAYER %in% player,
         # fullStats$SEASON >= input$Season[1], fullStats$SEASON <= input$Season[2],
-        # fullStats$TEAM %in% team
+        TEAM %in% team
         )%>%
-        ggplot(aes_string(input$XInput, input$YInput, colour=input$ColorBy))+geom_point()
-      
+    
+        ggplot(aes_string(input$XInput, input$YInput, colour=input$ColorBy))+geom_point()+geom_smooth()
+         
     })
 }
 
